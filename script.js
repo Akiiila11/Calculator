d902726c09b907eaef86ef918b232d64d1b73624
Akiiila11 <akilavenkatraj2001@gmail.com>
2025-08-26 12:17:32 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const 
+const result = document.getElementById("result")
+const btns = document.querySelectorAll(".up")
+const clear = document.getElementById("clear")
+const backspace = document.getElementById("backspace")
+const equal = document.getElementById("equal")
+
+btns.forEach (function(button){
+    button.addEventListener("click",function(){
+        result.value += result.textContent
+    })
+})
