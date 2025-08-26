cef372d8f266ec595fb57e44e6b9aa0de4fbcbb2
Akiiila11 <akilavenkatraj2001@gmail.com>
2025-08-26 16:30:32 +0530

    updated

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const result = document.getElementById("result")
-const btns = document.querySelectorAll(".up")
-const clear = document.getElementById("clear")
-const backspace = document.getElementById("backspace")
-const equal = document.getElementById("equal")
+const result = document.getElementById("result");
+const btns = document.querySelectorAll(".up");
+const clear = document.getElementById("clear");
+const backspace = document.getElementById("backspace");
+const equal = document.getElementById("equal");
 
-btns.forEach (function(button){
-    button.addEventListener("click",function(){
-        result.value += result.textContent
-    })
-})
+btns.forEach(function (button) {
+  button.addEventListener("click", function () {
+    result.value += button.textContent;
+  });
+});
+
+clear.addEventListener("click", () => {
+  result.value = "";
+});
+
+backspace.addEventListener("click", () => {
+  result.value = result.value.slice(0, -1);
+});
+
+equal.addEventListener("click", () => {
+  result.value = eval(result.value);
+});
